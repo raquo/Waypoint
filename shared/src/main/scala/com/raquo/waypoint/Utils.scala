b4c0ea4258a6aec30940f35613b2a22f43d01d9e
Nikita Gazarov <nikita@raquo.com>
2021-02-23 01:11:14 -0800

    Fix: Mitigate pf1.andThen(pf2) bug in Scala 2.12
    https://github.com/scala/scala/pull/7263

--- shared/src/main/scala/com/raquo/waypoint/Utils.scala
+++ shared/src/main/scala/com/raquo/waypoint/Utils.scala
@@ -10,4 +10,23 @@ object Utils {
   @inline private[waypoint] def absoluteUrlMatchesOrigin(origin: String, url: String): Boolean = {
     url.startsWith(origin + "/")
   }
+
+  // @TODO[Scala3] The isDefinedOf method of pf1.andThen(pf2) is broken in Scala 2.12, it does not consider pf2.isDefinedOf
+  //  - This is fixed in Scala 2.13, but until then we provide an implementation similar to 2.13 (with the same tests as in scala/scala)
+  //  - https://github.com/scala/scala/pull/7263
+  @inline private[waypoint] def andThenPF[A, B, C](pf: PartialFunction[A, B], k: PartialFunction[B, C]): PartialFunction[A, C] = {
+    new PartialFunction[A, C] with Serializable {
+      def isDefinedAt(x: A): Boolean = {
+        val b = pf.andThen[Option[B]](Some(_)).applyOrElse(x, (_: A) => None)
+        b.exists(k.isDefinedAt)
+      }
+
+      def apply(x: A): C = k(pf(x))
+
+      override def applyOrElse[A2 <: A, C1 >: C](x: A2, default: A2 => C1): C1 = {
+        val pfv = pf.andThen[Option[B]](Some(_)).applyOrElse(x, (_: A2) => None)
+        pfv.map(v => k.applyOrElse(v, (_: B) => default(x))).getOrElse(default(x))
+      }
+    }
+  }
 }
--- shared/src/main/scala/com/raquo/waypoint/package.scala
+++ shared/src/main/scala/com/raquo/waypoint/package.scala
@@ -14,14 +14,4 @@ package object waypoint extends Waypoint(
   type PatternArgs[PathArgs, QueryArgs] = UrlMatching[PathArgs, QueryArgs]
 
   @inline def PatternArgs: UrlMatching.type = UrlMatching
-
-  /** You can import waypoint.simple._ to use URL-DSl Simple* errors,
-    * but make sure you DO NOT import waypoint._ in this case, otherwise
-    * you will get ambiguous implicits.
-    */
-  //lazy val simple: Waypoint[SimplePathMatchingError, SimpleParamMatchingError, SimpleFragmentMatchingError] = new Waypoint(
-  //  SimplePathMatchingError.pathMatchingError,
-  //  SimpleParamMatchingError.itIsParamMatchingError,
-  //  SimpleFragmentMatchingError.itIsFragmentMatchingError
-  //)
 }
