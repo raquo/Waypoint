be021b4b3c1fa67ed4c403b683f060d29f703414
Artūras Šlajus <x11@arturaz.net>
2025-04-11 18:51:51 +0300

    Update WaypointException.scala

--- shared/src/main/scala/com/raquo/waypoint/WaypointException.scala
+++ shared/src/main/scala/com/raquo/waypoint/WaypointException.scala
@@ -1,9 +1,3 @@
 package com.raquo.waypoint
 
-/**
- * Prefixes the exception message to make it clear that this is a Waypoint exception.
- *
- * When you are in ScalaJS production build and the source maps are not loaded, it is quite hard
- * to tell where an exception comes from.
- */
 class WaypointException(message: String) extends Exception(s"[Waypoint] $message")
